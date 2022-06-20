e078ece95021cb145b0f8cadf9d8a91083ae7211
Joaquin Gutierrez <joacoo101@gmail.com>
2022-06-20 19:44:40 -0300

    Inicializacion NavBar

---
+++ src/Components/Header/NavBar.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Logo from "../../imgs/logo.png";
+import { FaShoppingCart } from "react-icons/fa";
+
+
+const Navbar = () =>{
+    return (
+        <header>
+            <nav>
+                <div class="nav-wrapper black">
+                    <div className="container">
+                        <a href="#" class="brand-logo"><img style={styles.imagen} src={Logo} alt="" /></a>
+                        <ul id="nav-mobile" class="right hide-on-med-and-down">
+                            <li><a href="">Cat 1</a></li>
+                            <li><a href="">Cat 2</a></li>
+                            <li><a href="">Cat 3</a></li>
+                            <FaShoppingCart />
+                        </ul>
+                        
+                    </div>
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+const styles = {
+    imagen: {
+        width: '50%',
+        margin: '5px',
+    }
+}
+
+
+export default Navbar
